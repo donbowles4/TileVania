b4ac753911124d6cf379cfae33128fe6593221e4
Donald Bowles <donniebowles84@gmail.com>
2022-04-06 14:13:42 -0400

    Prevent Wall Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,14 +12,16 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    CapsuleCollider2D myCapsuleCollider;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
     float fltGravityScaleAtStart;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         fltGravityScaleAtStart = myRigidBody.gravityScale;
     }
 
@@ -37,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){return;}
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){return;}
         if(value.isPressed)
         {
             myRigidBody.velocity += new Vector2 (0f, fltJumpSpeed);
@@ -64,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void ClimbLadder()
         {
-            if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+            if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
             {
                 myRigidBody.gravityScale = fltGravityScaleAtStart;
                 myAnimator.SetBool("blnIsClimbing", false);
