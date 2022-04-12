987e03c8b69959a7f321653fc16a037905c37f49
Donald Bowles <donniebowles84@gmail.com>
2022-04-12 15:12:38 -0400

    Level Exit Portal

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float fltLevelLoadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(fltLevelLoadDelay);
+        int intCurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int intNextSceneIndex = intCurrentSceneIndex + 1;
+
+        if(intNextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            intNextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(intNextSceneIndex);
+    }
+}
